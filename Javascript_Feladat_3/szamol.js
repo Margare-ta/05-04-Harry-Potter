c73e66f0923d14ff62fce27dfa800e0aee3a1891
Margareta <margaretavida120@gmail.com>
2021-05-09 19:24:12 +0200

    hááááááááát

--- Javascript_Feladat_3/szamol.js
+++ Javascript_Feladat_3/szamol.js
@@ -1,11 +1,50 @@
 let Kvaff = document.getElementById("myInput").value = "0";
 let Arany = document.getElementById("myin").value = "0";
+let bed = document.getElementById("bedobas");
+let elkap = document.getElementById("elkap");
 
 
+bed.addEventListener("click", valueee);
+elkap.addEventListener("click", arany);
 
-if(Kvaff.value < 0){
-  Kvaff.innerHTML = "0";
+function valueee(){
+    for (let szam = 0; szam < 150; szam++){
+
+
+      function fact(szam) { 
+        if (szam < 0){
+
+            return 0;
+         }else{
+           return ('szam' + 1);
+         }}
+        }
+
+    document.getElementById("myInput").value = ('fact');
 }
+
+function arany(){
+  for (let szam2 = 0; szam2 < 2; szam2++ ) {
+
+    function fact(szam2) { 
+      if (szam2 < 0){
+
+          return 0;
+
+       }else{
+         return ('szam2' + 1);
+       }}
+
+       if (szam2 > 1){
+        window.alert("Vége a játéknak!");
+      }
+      }
+
+  document.getElementById('myin').value = ('szam2');
+}
+
+
+
 
 
 
