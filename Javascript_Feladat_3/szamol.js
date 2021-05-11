a2101e2888d1606fedb6635d11b74582f787b04f
Margareta <margaretavida120@gmail.com>
2021-05-11 12:52:21 +0200

    sok sok segítség után

--- Javascript_Feladat_3/szamol.js
+++ Javascript_Feladat_3/szamol.js
@@ -1,44 +1,42 @@
-let Kvaff = document.getElementById("myInput").value = "0";
-let Arany = document.getElementById("myin").value = "0";
+let Kvaff = document.getElementById("myInput");
+let Arany = document.getElementById("myin").value = 0;
 let bed = document.getElementById("bedobas");
 let elkap = document.getElementById("elkap");
 let h2 = document.getElementById("h2");
 
+let pontszam = 0;
+
 bed.addEventListener("click", valueee);
 elkap.addEventListener("click", arany);
 
-function valueee(){
-    for (let szam = 0; szam < 150; szam++){
-
-
-      function fact(szam) { 
-        if (szam < 0){
-
-            return 0;
-         }else{
-           return ('szam' + 1);
-         }}
-        }
-
-    document.getElementById("myInput").value = ('fact');
+function valueee() {
+  let ertek = document.getElementById("myInput").value;
+  if(ertek < 0){
+    alert("A megadott érték nem lehet negatív!");
+  } else{
+    Kvaff.value++;
+    pontszam += 10;
+    h2.innerText = "Összesen: " + pontszam + " pont";
+  }
 }
 
-function arany(){
-  for (let szam2 = 0; szam2 < 3; szam2++ ) {
+function arany() {
+  for (let szam2 = 0; szam2 < 3; szam2++) {
 
-    function fact(szam2) { 
-      if (szam2 < 0){
+    function fact(szam2) {
+      if (szam2 < 0) {
 
-          return 0;
+        return 0;
 
-       }else{
-         return ('szam2' + 1);
-       }}
-
-       if (szam2 > 1){
-        window.alert("Vége a játéknak!");
+      } else {
+        return ('szam2' + 1);
       }
-      }
+    }
+
+    if (szam2 > 1) {
+      window.alert("Vége a játéknak!");
+    }
+  }
 
   document.getElementById('myin').value = ('szam2');
 }
